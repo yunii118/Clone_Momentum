0e96a09ddf3b669d8022afb3811231401879899d
yunii118 <lyhee118@gmail.com>
2023-12-01 18:34:43 +0900

    시계 설정, 인용구

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,10 @@ const clock = document.querySelector("#clock");
 
 function getClock(){
     const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`
 }
 // interval : 반드시 일어나야하는것. 매 2초마다 라곻 설정하면 2초마다 해당 함수가 실행되어야함
 // setInterval(실행할 함수, ms)
